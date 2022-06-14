ce3cab7c5553cff68c2d6ce8629ab181b4cf0d27
ddubinin <dubinin.dmitry@mail.ru>
2022-06-14 14:28:29 +0300

    feat: create new routes for users

--- controllers/users.js
+++ controllers/users.js
@@ -20,8 +20,28 @@ const createUser = (req, res) => {
     .catch((err) => res.send({ message: err.message }));
 };
 
+const updateUser = (req, res) => {
+  const { name, about } = req.body;
+  const id = req.user._id;
+
+  User.findByUdAndUpdate({ name, about, id }, { new: true })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.send({ message: err.message }));
+};
+
+const updateAvatar = (req, res) => {
+  const { avatar } = req.body;
+  const id = req.user._id;
+
+  User.findByUdAndUpdate({ avatar, id }, { new: true })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.send({ message: err.message }));
+};
+
 module.exports = {
   getUsers,
   getUser,
   createUser,
+  updateUser,
+  updateAvatar,
 };
