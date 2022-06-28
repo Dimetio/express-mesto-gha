69328a21d8f484cbd115090d2b270956477b32af
ddubinin <dubinin.dmitry@mail.ru>
2022-06-28 17:45:22 +0300

    feat: user control when delete a card

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,4 +1,9 @@
-const { BAD_REQUEST, NOT_FOUND, SERVER_ERROR } = require('../utils/error_code');
+const {
+  BAD_REQUEST,
+  NOT_FOUND,
+  SERVER_ERROR,
+  FORBIDDEN_ERROR,
+} = require('../utils/error_code');
 const Card = require('../models/card');
 
 const getCards = (req, res) => {
@@ -23,21 +28,30 @@ const createCard = (req, res) => {
 };
 
 const deleteCard = (req, res) => {
-  Card.findByIdAndRemove(req.params.cardId)
+  Card.findById(req.params._id)
     .then((card) => {
-      if (!card) {
-        res.status(NOT_FOUND).send({ message: 'Место не найдено' });
+      if (card.owner !== req.user._id) {
+        res.status(FORBIDDEN_ERROR).send({ message: 'Недостаточно прав' });
         return;
       }
-      res.send({ data: card });
+
+      Card.findByIdAndRemove(req.params.cardId)
+        .then((cardData) => {
+          if (!cardData) {
+            res.status(NOT_FOUND).send({ message: 'Место не найдено' });
+            return;
+          }
+          res.send({ data: cardData });
+        })
+        .catch((err) => {
+          if (err.path === '_id') {
+            res.status(BAD_REQUEST).send({ message: 'Неправильный id' });
+          } else {
+            res.status(SERVER_ERROR).send({ message: 'Ой! Что-то пошло не так, мы скоро поправим' });
+          }
+        });
     })
-    .catch((err) => {
-      if (err.path === '_id') {
-        res.status(BAD_REQUEST).send({ message: 'Неправильный id' });
-      } else {
-        res.status(SERVER_ERROR).send({ message: 'Ой! Что-то пошло не так, мы скоро поправим' });
-      }
-    });
+    .catch();
 };
 
 const addLike = (req, res) => {
